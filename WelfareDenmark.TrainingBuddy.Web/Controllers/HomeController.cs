0529088039afbe2f4ebea953bd4d16c8ac60e3bf
AndreasIT <32296603+AndreasIT@users.noreply.github.com>
2018-09-27 09:10:10 +0200

    Login modal oversat til dansk. login.cshtml slettet. login metode slettet fra HomeController

--- WelfareDenmark.TrainingBuddy.Web/Controllers/HomeController.cs
+++ WelfareDenmark.TrainingBuddy.Web/Controllers/HomeController.cs
@@ -15,11 +15,6 @@ namespace WelfareDenmark.TrainingBuddy.Web.Controllers
             return View();
         }
 
-        public IActionResult Login()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
