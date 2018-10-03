f50708048d9472bf2ee234f9035435736f1b6d94
alex2422 <alex2422@edu.eal.dk>
2018-10-03 11:10:26 +0200

    Started on implementing friend list view component

---
+++ WelfareDenmark.TrainingBuddy.Web/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace WelfareDenmark.TrainingBuddy.Web.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ WelfareDenmark.TrainingBuddy.Web/ViewComponents/FriendListViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WelfareDenmark.TrainingBuddy.Web.Models;
+
+namespace WelfareDenmark.TrainingBuddy.Web.ViewComponents
+{
+    public class FriendListViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            FriendListViewModel friendListViewModel = new FriendListViewModel
+            {
+                Users = new List<User>
+                {
+                    new User { Name = "Gerda Larsen" },
+                    new User { Name = "Karsten Olsen" },
+                    new User { Name = "Kim Nielsen" },
+                    new User { Name = "Morten Pedersen" },
+                    new User { Name = "Hanne Holm" }
+                }
+            };
+
+
+            return View(friendListViewModel);
+        }
+    }
+}
