b3111c8a2a6b81f80378efd0b5b8d1a83cba6924
Simon Egebjerg Jensen <simo897d@edu.eal.dk>
2018-10-10 09:21:15 +0200

    New site CallView/CallSite Showing webcam automatically when entering the page

---
+++ WelfareDenmark.TrainingBuddy.Web/Controllers/CallViewController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WelfareDenmark.TrainingBuddy.Web.Controllers
+{
+    public class CallViewController : Controller
+    {
+        public IActionResult CallSite()
+        {
+            return View();
+        }
+    }
+}
