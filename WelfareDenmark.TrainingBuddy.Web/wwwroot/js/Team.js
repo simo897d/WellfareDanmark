a2364c261ebf8cf83be2d628e81e0542462ba1d3
Falke Glavind Ramstedt <falk0016@edu.eal.dk>
2018-11-27 12:52:17 +0100

    Tilføjet funktion til at flytte objekter mellem listboxe i opret hold.

---
+++ WelfareDenmark.TrainingBuddy.Web/wwwroot/js/Team.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $('#btnRight').click(function (e) {
+        var selectedOpts = $('#lstBox1 option:selected');
+        if (selectedOpts.length == 0) {
+            alert("Nothing to move.");
+            e.preventDefault();
+        }
+
+        $('#lstBox2').append($(selectedOpts).clone());
+        $(selectedOpts).remove();
+        e.preventDefault();
+    });
+
+    $('#btnLeft').click(function (e) {
+        var selectedOpts = $('#lstBox2 option:selected');
+        if (selectedOpts.length == 0) {
+            alert("Nothing to move.");
+            e.preventDefault();
+        }
+
+        $('#lstBox1').append($(selectedOpts).clone());
+        $(selectedOpts).remove();
+        e.preventDefault();
+    });
+});
