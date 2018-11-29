965114957f976572e78163c173d89d42effc241c
Simon Egebjerg Jensen <simo897d@edu.eal.dk>
2018-11-29 10:39:36 +0100

    Can see your own webcam using webRTC instead of plain HTML

---
+++ WelfareDenmark.TrainingBuddy.Web/wwwroot/js/VideoChatService.js
@@ -0,0 +1,68 @@
+﻿'use strict';
+
+
+// On this codelab, you will be streaming only video (video: true).
+const mediaStreamConstraints = {
+    video: true,
+};
+
+// Video element where stream will be placed.
+const localVideo = document.querySelector('video');
+
+// Local stream that will be reproduced on the video.
+let localStream;
+
+// Handles success by adding the MediaStream to the video element.
+function gotLocalMediaStream(mediaStream) {
+    localStream = mediaStream;
+    localVideo.srcObject = mediaStream;
+}
+
+// Handles error by logging a message to the console with the error message.
+function handleLocalMediaStreamError(error) {
+    console.log('navigator.getUserMedia error: ', error);
+}
+
+// Initializes media stream.
+navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+    .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
+
+let localPeerConnection;
+
+localPeerConnection = new RTCPeerConnection(servers);
+localPeerConnection.addEventListener('icecandidate', handleConnection)
+localPeerConnection.addEventListener('iceconnectionstatechange', handleConnectionChange)
+
+navigator.mediaDevices.getUserMedia(mediaStreamConstraints).
+    then(gotLocalMediaStream).
+    catch(handleLocalMediaStreamError);
+
+function gotLocalMediaStream(mediaStream) {
+    localVideo.srcObject = mediaStream;
+    localStream = mediaStream;
+    trace('Received local stream');
+    callButton.disabled = false;
+}
+
+localPeerConnection.addStream(localStream);
+trace('Added local stream to localPeerConnection');
+
+function handleConnection(event) {
+    const peerConnection = event.target;
+    const iceCandidate = event.candidate;
+
+    if (iceCandidate) {
+        const newIceCandidate = new RTCIceCandidate(iceCandidate);
+        const otherPeer = getOtherPeer(peerConnection);
+
+        otherPeer.addIceCandidate(newIceCandidate)
+            .then(() => {
+                handleConnectionSuccess(peerConnection);
+            }).catch((error) => {
+                handleConnectionFailure(peerConnection, error);
+            });
+
+        trace(`${getPeerName(peerConnection)} ICE candidate:\n` +
+            `${event.candidate.candidate}.`);
+    }
+}
