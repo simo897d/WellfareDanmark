868d7215c6a35e4ab6aa150beaa6f50889a14f99
Simon Egebjerg Jensen <simo897d@edu.eal.dk>
2018-11-29 12:41:00 +0100

    Socket.io implementation

--- WelfareDenmark.TrainingBuddy.Web/wwwroot/js/VideoChatService.js
+++ WelfareDenmark.TrainingBuddy.Web/wwwroot/js/VideoChatService.js
@@ -1,6 +1,35 @@
 ﻿'use strict';
 
+var isInitiator;
 
+window.room = prompt("Enter room name:");
+
+var socket = io.connect();
+
+if (room !== "") {
+    console.log("Message from client: Asking to join room" + room);
+    socket.emit('create or join', room)
+}
+
+socket.on('created', function (room, clientId) {
+    isInitiator = true;
+});
+
+socket.on('full', function (room) {
+    console.log('Message from client: room ' + room + 'is full');
+});
+
+socket.on('ipaddr', function (ipaddr) {
+    console.log('Message from client: Server IP adress is' + ipaddr)
+});
+
+socket.on('joined', function (room, clientId) {
+    isInitiator = false;
+});
+
+socket.on('log', function (array) {
+    console.log.apply(console, array);
+});
 // On this codelab, you will be streaming only video (video: true).
 const mediaStreamConstraints = {
     video: true,
