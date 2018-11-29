f1310b3908fc3e54fb8f1ceb402446379c6221f5
Simon Egebjerg Jensen <simo897d@edu.eal.dk>
2018-11-29 11:31:09 +0100

    Dno. Trying to make connection with RTC. Not working as intended

--- WelfareDenmark.TrainingBuddy.Web/wwwroot/js/VideoChatService.js
+++ WelfareDenmark.TrainingBuddy.Web/wwwroot/js/VideoChatService.js
@@ -66,3 +66,88 @@ function handleConnection(event) {
             `${event.candidate.candidate}.`);
     }
 }
+
+trace('localPeerConnection createOffer start.');
+localPeerConnection.createOffer(offerOptions)
+    .then(createdOffer).catch(setSessionDescriptionError);
+
+function createdOffer(description) {
+    trace(`Offer from localPeerConnection:\n${description.sdp}`);
+
+    trace('localPeerConnection setLocalDescription start.');
+    localPeerConnection.setLocalDescription(description)
+        .then(() => {
+            setLocalDescriptionSuccess(localPeerConnection);
+        }).catch(setSessionDescriptionError);
+
+    trace('remotePeerConnection setRemoteDescription start.');
+    remotePeerConnection.setRemoteDescription(description)
+        .then(() => {
+            setRemoteDescriptionSuccess(remotePeerConnection);
+        }).catch(setSessionDescriptionError);
+
+    trace('remotePeerConnection createAnswer start.');
+    remotePeerConnection.createAnswer()
+        .then(createdAnswer)
+        .catch(setSessionDescriptionError);
+}
+
+// Logs answer to offer creation and sets peer connection session descriptions.
+function createdAnswer(description) {
+    trace(`Answer from remotePeerConnection:\n${description.sdp}.`);
+
+    trace('remotePeerConnection setLocalDescription start.');
+    remotePeerConnection.setLocalDescription(description)
+        .then(() => {
+            setLocalDescriptionSuccess(remotePeerConnection);
+        }).catch(setSessionDescriptionError);
+
+    trace('localPeerConnection setRemoteDescription start.');
+    localPeerConnection.setRemoteDescription(description)
+        .then(() => {
+            setRemoteDescriptionSuccess(localPeerConnection);
+        }).catch(setSessionDescriptionError);
+}
+function createConnection() {
+    dataChannelSend.placeholder = '';
+    var servers = null;
+    pcConstraint = null;
+    dataConstraint = null;
+    trace('Using SCTP based data channels');
+    // For SCTP, reliable and ordered delivery is true by default.
+    // Add localConnection to global scope to make it visible
+    // from the browser console.
+    window.localConnection = localConnection =
+        new RTCPeerConnection(servers, pcConstraint);
+    trace('Created local peer connection object localConnection');
+
+    sendChannel = localConnection.createDataChannel('sendDataChannel',
+        dataConstraint);
+    trace('Created send data channel');
+
+    localConnection.onicecandidate = iceCallback1;
+    sendChannel.onopen = onSendChannelStateChange;
+    sendChannel.onclose = onSendChannelStateChange;
+
+    // Add remoteConnection to global scope to make it visible
+    // from the browser console.
+    window.remoteConnection = remoteConnection =
+        new RTCPeerConnection(servers, pcConstraint);
+    trace('Created remote peer connection object remoteConnection');
+
+    remoteConnection.onicecandidate = iceCallback2;
+    remoteConnection.ondatachannel = receiveChannelCallback;
+
+    localConnection.createOffer().then(
+        gotDescription1,
+        onCreateSessionDescriptionError
+    );
+    startButton.disabled = true;
+    closeButton.disabled = false;
+}
+
+function sendData() {
+    var data = dataChannelSend.value;
+    sendChannel.send(data);
+    trace('Sent Data: ' + data);
+}
