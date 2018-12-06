e5c86816c742e411a7460651b30f23c1a1ace77e
Simon Egebjerg Jensen <simo897d@edu.eal.dk>
2018-12-06 09:07:17 +0100

    dsf

--- WelfareDenmark.TrainingBuddy.Web/Models/CallViewViewModel.cs
+++ WelfareDenmark.TrainingBuddy.Web/Models/CallViewViewModel.cs
@@ -6,4 +6,36 @@ using System.Threading.Tasks;
 namespace WelfareDenmark.TrainingBuddy.Web.Models {
     public class CallViewViewModel {
     }
+
+    public class VideoStream {
+
+    }
+    //    public class MyYouTubeVideoObject {
+    //        public string VideoId { get; set; }
+    //        public string Title { get; set; }
+    //    }
+    //public class YouTubeVideo {
+    //    const string MYYOUTUBE_CHANNEL = "SampleChannel";
+    //    const string MyYOUTUBE_DEVELOPER_KEY =
+    //       "AIzaSyDsCC8-hnOokLB2qsBq3WnJXQ_9poKOBus";
+        
+    //}
+    //        public static MyYouTubeVideoObject[] GetVideos() {
+    //            YouTubeRequestSettings settings =
+    //               new YouTubeRequestSettings("SampleChannel",
+    //               MyYOUTUBE_DEVELOPER_KEY);
+    //            YouTubeRequest request = new YouTubeRequest(settings);
+    //            string feedUrl = String.Format
+    //               ("http://gdata.youtube.com/feeds/api/users/{0}
+    //               / uploads ? orderby = published", MYYOUTUBE_CHANNEL);
+       
+    //            Feed < Video > videoFeed = request.Get<Video>
+    //               (new Uri(feedUrl));
+    //            return (from video in videoFeed.Entries
+    //                    select new MyYouTubeVideoObject() { VideoId = video.VideoId, Title = video.Title })
+    //               .ToArray();
+
+    //        }
+
+        //}
 }
